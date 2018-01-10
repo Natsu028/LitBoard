4436ca5822d7330d1f044ba50b83afea4489dc5a
Natsu028 <orangemikan.028@gmail.com>
2018-01-11 00:27:57 +0900

    作成中...

---
+++ app/src/main/java/Post.java
@@ -0,0 +1,9 @@
+/**
+ * Created by kodakanatsumi on 2018/01/10.
+ */
+
+public class Post {
+
+    String message;
+    String userName;
+}
---
+++ app/src/main/java/com/lifeistich/android/litboard/PostActivity.java
@@ -0,0 +1,47 @@
+package com.lifeistich.android.litboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class PostActivity extends AppCompatActivity
+        implements View.OnClickListener{
+
+    //firebase用
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference refMsg = database.getReference("message");
+    //refMsgはfirebase独自の変数　→処理の簡略化可能！
+    //'DatabaseReferance'とは何ぞや？
+    //'Reference'は参照という意味！
+
+    //Viewの構成要素
+    EditText mUsernameText;
+    EditText mPostText;
+    Button mPostButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+
+        mUsernameText = (EditText) findViewById(R.id.bookname);
+        mPostText = (EditText) findViewById(R.id.message);
+        mPostButton = (Button) findViewById(R.id.post);
+    }
+    @Override
+    public void onClick(View v){
+        int id = v.getId();
+
+        switch (id){
+            case R.id.post://コロン: と　セミコロン; の違いは？
+                //TODO:投稿ボタンを押した時の処理
+                break;
+        }
+    }
+}
